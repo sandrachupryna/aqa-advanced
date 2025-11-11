f0e654c4e2b7683c927f9ef2e37517ac084fb63c
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2025-11-11 22:08:36 +0200

    Add task2: strings

---
+++ js-basics/task2.js
@@ -0,0 +1,5 @@
+const firstPersonName = "Angelina Jolie";
+const secondPersonName = "Brad Pitt";
+
+console.log('The main actors in the movie \"Mr. & Mrs. Smith\" are  ' + firstPersonName + ' and ' + secondPersonName + '.');
+console.log(`The actors ${firstPersonName} and ${secondPersonName} are no longer a couple.`);
