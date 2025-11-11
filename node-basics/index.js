c109c64af8af479c66e79c078a40411750aa27be
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2025-11-11 17:50:41 +0200

    Add index.js with chalk styling examples

---
+++ node-basics/index.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+// Nest styles of the same type even (color, underline, background)
+console.log(chalk.cyanBright.strikethrough(
+	'I am a cyan line ' +
+	chalk.yellow.overline.underline.bold.italic('with a yellow substring') +
+	' that becomes cyan again!'
+));
+
+console.log(chalk.magenta.strikethrough(
+	'I am a magenta line ' +
+	chalk.reset.yellow.overline.underline.bold.italic('with a yellow substring') +
+	' that doesn\'t become magenta again because of the \'reset\'!'
+));
+
+console.log(chalk.bgRed.green.bold('I am a green line on a red background, but my color is white because of the background property!'));
+
+console.log(chalk.bgHex('#b522baff')('I am a text with a bg color set using a hex code!', 
+            chalk.dim(' (and I am nested dimmed part)'), 'Wow!'));
