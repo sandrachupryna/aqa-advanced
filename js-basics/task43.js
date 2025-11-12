68d85b03dc9674b5cbe4935c36129956eb8e6ba6
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2025-11-12 16:15:39 +0200

    Add task4: mathematical operations

---
+++ js-basics/task43.js
@@ -0,0 +1,5 @@
+const radius = 3.27;
+const height = 10.5;
+let volume = Math.PI * Math.pow(radius, 2) * height;
+
+console.log(`The volume of a cylinder with radius ${radius} and height ${height} is: ${volume.toFixed(2)}`);
