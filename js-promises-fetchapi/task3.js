08a5bcf8606908664ee2243da2ffb5635efd9bba
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2025-12-14 21:56:20 +0200

    Add solution of HW#11 tasks: promises and fetchAPI

---
+++ js-promises-fetchapi/task3.js
@@ -0,0 +1,44 @@
+async function getToDo(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        if (!response.ok) {
+            throw new Error(`HTTP error, status: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+  } catch (error) {
+        console.error('Error fetching "todo" object:', error);
+        throw error; // need this row for rejected status 
+  }
+}
+
+async function getUser(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        if (!response.ok) {
+            throw new Error(`HTTP error, status: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+  } catch (error) {
+        console.error('Error fetching "user" object:', error);
+        throw error; // need this row for rejected status 
+  }
+}
+
+
+// To check result of both promises
+const promiseCollectionSettled =  Promise.allSettled([getToDo(), getUser()]);
+promiseCollectionSettled.then( x => console.log('All Settled:', x) );
+
+const promiseCollectionAll =  Promise.all([getToDo(), getUser()]);
+promiseCollectionAll
+    .then(x => console.log('Promise.All Resolved:', x))
+    .catch(error => console.error('Promise.All Rejected:', error));
+
+const promiseCollectionRace = Promise.race([getToDo(), getUser()]);
+promiseCollectionRace
+    .then(x => console.log('Promise.Race Winner:', x))
+    .catch(error => console.error('Promise.Race Rejected:', error));
+
+
